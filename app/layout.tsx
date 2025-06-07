ee45ec452d8d5ee30dee200a2fda76edc879c7c1
Nayak <rahul.nyk@gmail.com>
2025-06-07 18:07:31 +0530

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,40 +2,59 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { hasEnvVars } from "@/lib/utils";
+import { EnvVarWarning } from "@/components/env-var-warning";
+import { AuthButton } from "@/components/auth-button";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+    metadataBase: new URL(defaultUrl),
+    title: "Next.js and Supabase Starter Kit",
+    description: "The fastest way to build apps with Next.js and Supabase",
 };
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  display: "swap",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    display: "swap",
+    subsets: ["latin"],
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={`${geistSans.className} antialiased`}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <main className="min-h-screen flex flex-col items-center">
+                        {/* <div className="flex-1 w-full flex flex-col gap-20 items-center"> */}
+                        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 sticky top-0 bg-background z-50">
+                            <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                                <ThemeSwitcher />
+                                {!hasEnvVars ? (
+                                    <EnvVarWarning />
+                                ) : (
+                                    <AuthButton />
+                                )}
+                            </div>
+                        </nav>
+                        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+                            {children}
+                        </div>
+                    </main>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
