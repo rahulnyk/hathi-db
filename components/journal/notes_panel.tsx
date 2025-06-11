6fa2115a8714a40024a8c6159e50880a0864f9de
Nayak <rahul.nyk@gmail.com>
2025-06-11 14:08:34 +0530

    intrim

--- components/journal/notes_panel.tsx
+++ components/journal/notes_panel.tsx
@@ -3,8 +3,12 @@
 import { NotesEditor } from "./notes_editor";
 import { Thread } from "./thread";
 import { cn } from "@/lib/utils";
+import { useAppSelector } from "@/store";
+import { NotebookPen } from "lucide-react"; // Assuming you have a Disc icon from lucide-react
 
 export function NotesPanel({ hidden = false }: { hidden?: boolean }) {
+    const { currentContext } = useAppSelector((state) => state.notes);
+
     return (
         <div
             className={cn(
@@ -13,7 +17,32 @@ export function NotesPanel({ hidden = false }: { hidden?: boolean }) {
                 hidden ? "hidden lg:flex" : "flex w-full" // Mobile: hidden if suggestions shown, else full width flex. LG: respects lg:flex
             )}
         >
-            <div className="flex flex-col w-full relative gap-6 md:gap-8 lg:gap-12">
+            <div className="flex flex-col w-full relative gap-6 md:gap-8 lg:gap-8">
+                <div
+                    className={cn(
+                        "flex flex-row w-full relative mx-4 justify-start items-center gap-4",
+                        "text-zinc-400 dark:text-zinc-500"
+                    )}
+                >
+                    {/* Context header */}
+                    <NotebookPen size={24} />
+                    <h2
+                        className={cn(
+                            "text-2xl font-extrabold inline-block"
+                            // "bg-gradient-to-r from-zinc-500 to-zinc-700",
+                            // "bg-clip-text text-transparent"
+                        )}
+                    >
+                        {currentContext
+                            .split("-")
+                            .map(
+                                (word) =>
+                                    word.charAt(0).toUpperCase() +
+                                    word.slice(1).toLowerCase()
+                            )
+                            .join(" ")}
+                    </h2>
+                </div>
                 {/* Note input section */}
                 <NotesEditor />
 
