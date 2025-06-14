6e66ae60654a3da9419bfb60f252d58840856df7
Nayak <rahul.nyk@gmail.com>
2025-06-14 19:11:53 +0530

    Chatbot like layout where the input box is at the bottom of the screen

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -11,12 +11,12 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(
         return (
             <TextareaAutosize
                 className={cn(
-                    "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm",
+                    "flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm",
                     "placeholder:text-muted-foreground",
-                    "min-h-48 mb-4 bg-transparent border-none outline-none",
+                    "min-h-24 mb-4 bg-transparent border-none outline-none",
                     "focus:ring-0 focus:border-none focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0",
                     "disabled:cursor-not-allowed disabled:opacity-50",
-                    "resize",
+                    "resize-none",
                     // "ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
                     className
                 )}
