d16927000155f23702591d5c1aff3e211400eb17
Nayak <rahul.nyk@gmail.com>
2025-06-19 21:38:10 +0530

    resolved copilot reviewer suggestions

--- store/uiSlice.ts
+++ store/uiSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-type DeviceType = "mobile" | "tablet" | "desktop";
+export type DeviceType = "mobile" | "tablet" | "desktop";
 
 interface UIState {
     deviceType: DeviceType;
