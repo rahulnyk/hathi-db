476a94472352a7556ff12c210b4f3c45cb806260
Nayak <rahul.nyk@gmail.com>
2025-06-19 20:13:53 +0530

    Add DeviceTypeDetector component and integrate it into RootLayout; update UI state management for device type

---
+++ components/device_type_detector.tsx
@@ -0,0 +1,22 @@
+"use client";
+// Example: _app.tsx or layout.tsx or a top-level component
+import { useEffect } from "react";
+import { useAppDispatch } from "@/store";
+import { setDeviceType } from "@/store/uiSlice";
+
+export function DeviceTypeDetector() {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        const userAgent = navigator.userAgent;
+        if (/Mobi/i.test(userAgent)) {
+            dispatch(setDeviceType("mobile"));
+        } else if (/Tablet/i.test(userAgent)) {
+            dispatch(setDeviceType("tablet"));
+        } else {
+            dispatch(setDeviceType("desktop"));
+        }
+    }, [dispatch]);
+
+    return null;
+}
